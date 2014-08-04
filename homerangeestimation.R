5b46bea801f27a195a02093feac8aee1a4d8ffbb
Dakota Benjamin <dakota.benjamin@gmail.com>
2014-08-04 10:02:13 -0400

    First Commit

---
+++ homerangeestimation.R
@@ -0,0 +1,30 @@
+# NC Coyote Home Range Estimation
+# Cleveland Metroparks Department of Planning, Design, and Natural Resources
+# Author: Dakota Benjamin
+# Github repo: http://github.com/dakotabenjamin/nc_homerange
+
+#This script will produce day/night home range estimations for the North Chagrin coyote project.
+
+# Initialization ----
+rm(list=ls())
+#load packages
+require(adehabitatHR)
+require(rgdal)
+require(RAtmosphere)
+
+#set it the the directory containing the folders of the shape file for the centroids
+setwd("C:/Users/Dakota/Documents/GitHub/nc_homerange")
+
+# Load data ----
+#Load the coyote data
+coyote <- readOGR("nc_coyote_centroids_dn","nc_coyote_centroids_dn")
+
+#Make a kernel HR Estimation for day and night ----
+kernel <- kernelUD(coyote[,2], h="href")
+#image(kernel)
+ver <- getverticeshr(kernel, 95)
+
+# write out to a shape file ----
+writeOGR(ver, "hr_coyote_centroids_dn", "hr_centroids_dn", "ESRI Shapefile")
+
+
